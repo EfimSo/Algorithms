c4aa225b9cd30ce4e9984a4591dada0a8479c649
Fima1 <efim6357@gmail.com>
2023-01-04 22:18:22 -0700

    Remove key

--- Two Pointers/SeparateDuplicates.py
+++ Two Pointers/SeparateDuplicates.py
@@ -13,7 +13,20 @@ def remove_duplicates(arr):
         i += 1
     return nextNonDuplicate
 
-def main():
+# given a list remove all instances of key by moving non-key elems to the front and
+# returning length of the subarray with no instances of key
+
+def remove_key(arr, key):
+    next_not_key = 0 # pointer to the last confirmed non-key element
+    i = 0
+    while i < len(arr):
+        if arr[i] != key:
+            arr[next_not_key] = arr[i]
+            next_not_key += 1
+        i += 1
+    return next_not_key
+
+def main1():
   l1 = [2, 3, 3, 3, 6, 9, 9]
   l2 = [2, 2, 2, 11]
   print(remove_duplicates(l1))
@@ -21,4 +34,15 @@ def main():
   print(remove_duplicates(l2))
   print(l2)
 
-main()
+# main1()
+
+def main2():
+  l1 = [3, 2, 3, 6, 3, 10, 9, 3]
+  print(remove_key(l1, 3))
+  print(l1)
+  l2 = [2, 11, 2, 2, 1]
+  print(remove_key(l2, 2))
+  print(l2)
+
+
+main2()
