326f4b0ab2fc32f8df1649f03c8f2838340b5b85
Fima1 <efim6357@gmail.com>
2023-01-05 23:58:07 -0700

    Closest Triplet To Target

--- Two Pointers/ClosestTripletToTarget.py
+++ Two Pointers/ClosestTripletToTarget.py
@@ -2,20 +2,23 @@
 # find a triplet in the array whose sum is as close to the target number as possible,
 # return the sum of the triplet.
 
+# time complexity: sort + algorithm == O(nlogn) + O(n^2) == O(n^2)
+# space complexity: O(n) —— worst case scenario for sorting 
+
 import math
 
 def triplet_sum_close_to_target(arr, target_sum):
     arr.sort()
-    min_diff = math.inf
+    min_diff = math.inf        # keep track of the smallest difference so far
 
-    for i in range(len(arr) - 2):
+    for i in range(len(arr) - 2):       # len - 2 since i, left and right have to be separate indeces
         left = i + 1
         right = len(arr) - 1
         while left < right:
             diff = target_sum - arr[i] - arr[left] - arr[right]
             if diff == 0:
                 return target_sum
-            if abs(diff) <  abs(min_diff) or abs(diff) == abs(diff) and diff > min_diff:
+            if abs(diff) <  abs(min_diff) or abs(diff) == abs(diff) and diff > min_diff:    
                 min_diff = diff
             if diff > 0:
                 left += 1
