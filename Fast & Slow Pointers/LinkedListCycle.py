bff10d055074637b8564c6a511d97649aa82f4bd
Fima1 <efim6357@gmail.com>
2023-01-12 00:01:12 -0700

    Cycle in a Linked List

---
+++ Fast & Slow Pointers/LinkedListCycle.py
@@ -0,0 +1,34 @@
+# Given the head of a Singly LinkedList,
+# write a function to determine if the LinkedList has a cycle in it or not.
+
+class Node:
+    def __init__(self, val, next = None):
+        self.value = val
+        self.next = next
+    
+def has_cycle(head):
+    slow, fast = head, head
+    while fast is not None and fast.next is not None:
+        fast = fast.next.next
+        slow = slow.next
+        if fast == slow:    # == would need to be 'is' if __eq__ was defined for Node 
+            return True
+    return False
+
+def main():
+  head = Node(1)
+  head.next = Node(2)
+  head.next.next = Node(3)
+  head.next.next.next = Node(4)
+  head.next.next.next.next = Node(5)
+  head.next.next.next.next.next = Node(6)
+  print("LinkedList has cycle: " + str(has_cycle(head)))
+
+  head.next.next.next.next.next.next = head.next.next
+  print("LinkedList has cycle: " + str(has_cycle(head)))
+
+  head.next.next.next.next.next.next = head.next.next.next
+  print("LinkedList has cycle: " + str(has_cycle(head)))
+
+
+main()
