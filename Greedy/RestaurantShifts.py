e5147c6bb5611ef571d5501de6e7e2eb913fb987
Fima1 <74836279+Fima1@users.noreply.github.com>
2023-03-01 19:33:12 -0500

    Update RestaurantShifts.py

--- Greedy/RestaurantShifts.py
+++ Greedy/RestaurantShifts.py
@@ -32,12 +32,29 @@ print(find_min_shifts(l))
 print(find_visits(l))
 
 
-# Online Python - IDE, Editor, Compiler, Interpreter
+class Node:
+    def __init__(key, val, left=None, right=None):
+        self.key = key
+        self.val = val
+        self.left = left
+        self.right = right
+    def __repr__():
+        return "(" + str(self.key) + "," + str(self.val) + ")"
 
-def find_smaller(root, val):
-    if not root or root.key > val:
+def find_smaller(root, t):
+    if not root or root.key >= t:
         return 0
-    sum = root.val
-    sum += find_smaller(root.left)
-    sum += find_smaller(root.right)
-    return sum
+    res = [root]
+    res += find_smaller(root.left)
+    res += find_smaller(root.right)
+    return res
+
+def main():
+    n1 = Node(3, 4)
+    n1.left = Node(5, 1)
+    n1.right = Node(6, 4)
+    n1.left.left = Node(7, "idk")
+    n1.right.left = Node(10, 8)
+    n1.right.right = Node(7, 5)
+    print(find_smaller(n1, 8)
+    
