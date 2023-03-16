3230266c4ae004bb8b86ae77bb5064b794c60677
Fima1 <efim6357@gmail.com>
2023-03-16 03:22:52 -0400

    Unique shortest paths, trains

---
+++ Greedy/Trains.py
@@ -0,0 +1,58 @@
+# Given n 4-tuples train(i) = (depcity(i), arrcity(i), dep(i), arr(i)) where
+# each tuple is a train leaving depcity(i) at dep(i) and arriving
+# to arrcity(i) at arr(i) and a starting city,
+# return the shortest time you can travel from your city to every other one
+# uses a version of Dijkstra with a min heap
+from heapq import heappush, heappop
+from math import inf
+from Dijkstra import Dijkstra
+
+def find_cities(cities, s): # s = starting city
+    G = {depcity: [] for depcity, *other in cities}
+    G.update({arrcity: [] for depcity, arrcity, *other in cities})
+    for depcity, arrcity, dept, arrt in cities:
+        G[depcity].append((arrcity, dept, arrt))
+    Q = [(0, s)]
+    set_cities = {s}
+    for  depcity, arrcity, dept, arrt in cities:
+        if depcity not in set_cities:
+            Q.append((inf, depcity))
+            set_cities.add(depcity)
+        if arrcity not in set_cities:
+            Q.append((inf, arrcity))
+            set_cities.add(arrcity)
+    # Q.extend([(inf, depcity) for depcity, *other in cities])
+    p = {depcity: 0 if depcity == s else inf for depcity, *other in cities} # earliest known time you can arrive at a city
+    p.update({arrcity: inf for depcity, arrcity, *other in cities if arrcity not in p})
+    dist = dict()
+    while Q:
+        earliest_arr_time, dep_city = heappop(Q)
+        dist[dep_city] = earliest_arr_time
+        if G[dep_city]:
+            for arr_city, dep_time, arr_time in G[dep_city]:
+                if dep_time >= earliest_arr_time:
+                    # try: Q.remove((p[arr_city], arr_city)) 
+                    # except ValueError: pass 
+                    Q.remove((p[arr_city], arr_city)) 
+                    p[arr_city] = arr_time
+                    heappush(Q, (p[arr_city], arr_city))
+    return dist
+
+def find_cities2(cities, s):
+    G = {depcity: [] for depcity, *other in cities}
+    G.update({arrcity: [] for depcity, arrcity, *other in cities if arrcity not in G})
+    for depcity, arrcity, dept, arrt in cities:
+        G[depcity].append((arrcity, arrt - dept))
+    d, parents = Dijkstra(G, s)
+    return d
+# have to set distance to the arrival time not previous time + difference
+# sort by arrival time instead of difference?
+def main():
+    start_city = 'A'
+    # train_schedule = [('A', 'B', 0, 5), ('A', 'C', 0, 10), ('B', 'C', 5, 8), ('C', 'D', 10, 15)]
+    # train_schedule = [('A', 'B', 0, 2), ('A', 'C', 0, 3), ('B', 'C', 2, 3), ('B', 'D', 2, 6), ('C', 'D', 3, 6)]
+    train_schedule = [('A', 'B', 0, 5), ('B', 'C', 5, 6), ('C', 'D', 10, 15), ('B', 'D', 4, 7)]
+
+    distances = find_cities(train_schedule, start_city)
+    print(distances)
+main()
---
+++ Greedy/UniquePaths2.py
@@ -0,0 +1,105 @@
+from Dijkstra import Dijkstra
+from math import inf
+import heapq as hq
+
+# algorithm that returns the edges of two distinct paths from s to x given graph G,
+# source node s and destination node x which is known to have multiple shortest paths from s
+# current issue: does not work correctly when backtracking only one node
+# correct reversing the list twice in DFS and find_two_shortest_paths
+def find_two_shortest_paths(G, s, x):
+    # helper function for edge printing    
+    def get_edges(nodes: list[str]):
+        res = []
+        for i in range(1, len(nodes)):
+            res.append((nodes[i-1], nodes[i]))
+        return res
+    # finds minimum path using Dijkstra
+    dist, parents = Dijkstra(G, s)
+    if dist[x] == inf:
+        return None, None   # x is not reachable from s
+    # prev = x
+    curr = parents[x]
+    # distance = 0
+    path1 = [x]
+    path_set = {x}
+    path2 = []
+    found = False
+    done = False
+    visited = set()
+    while curr is not None:
+        path1.append(curr)
+        path_set.add(curr)
+        if done:
+            path2.append(curr)
+        else:
+            print(path_set)
+            distance, path, found, node = DFS(G, curr, path_set, dist[curr], visited)
+            if found and distance == dist[node]:
+                print(f"node: {node}, distance: {distance}, dist: {dist[node]}")
+                done = True
+                print(f"path: {path}")
+                path2.extend(path)
+        # prev = curr
+        curr = parents[curr]
+    # return get_edges(path1[::-1]), get_edges(path2[::-1])
+    # print(dist[x])
+    return path1[::-1], path2[::-1]
+
+def DFS(G, s, pset, start_distance, visited):
+    # add path length?
+    def recur(u, weight, length, num_nodes, path, found):
+        print(f"node: {u}", f"path length: {num_nodes}")
+        if u in pset:
+            if num_nodes == 0:
+                pass 
+            elif num_nodes == 1:
+                return -1, False, None
+            else:
+                path.append(u)
+                return length + weight, True, u
+        visited.add(u)
+        if not G[u]: pass
+        else:
+            for v, wv in G[u]:
+                if v not in visited:
+                    l, f, n = recur(v, wv, length, num_nodes + 1, path, found)
+                    if f:
+                        found = True
+                        length = l
+                        u = n
+                        break
+        if found:
+            path.append(u)
+            return length + weight, True, u
+        else:
+            visited.remove(u)
+            return length, False, None
+    path = []
+    length, found, node = recur(s, 0, 0, 0, path, False)
+    if found:
+        return start_distance + length, path, True, node
+    else:
+        return -1, [], False, None
+
+
+def main():
+    G = dict()
+    G["s"] = [("a", 8), ("b", 6), ("c", 2)]
+    G["a"] = []
+    G["b"] = [("a", 2), ("e", 2)]
+    G["c"] = [("d", 8), ("e", 6)]
+    G["d"] = [("a", 3)]
+    G["e"] = [("d", 1)]
+    # dist, parents, unique = Dijkstra_unique(G, "a")
+    # print(f"Distances: {dist}")
+    # print(f"Parents: {parents}")
+    # print(f"Uniqueness: {unique}")
+
+    # path, dist = find_min_path(G, "s", "d")
+    # print(f"path: {path}")
+    # print(f"distance: {dist}") 
+    # visited = set()
+    # print(DFS(G, "s", "c", 0, visited))
+    p1, p2 = find_two_shortest_paths(G, "s", "e")
+    print(p1, p2)
+main()
