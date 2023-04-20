1e56d1b68db26dc7e03b7bdd2513979c7893cd68
Fima1 <efim6357@gmail.com>
2023-04-19 21:37:24 -0400

    Bellman-Ford

--- Dynamic Programming/Bellman-Ford.py
+++ Dynamic Programming/Bellman-Ford.py
@@ -1,6 +1,8 @@
 # implementation of the Bellman-Ford algorithm
 # given a destination vertex, finds the shortest path from each vertex to the destination vertex
 from math import inf
+# finds minimal distances to reach t from each node v != t in G
+# M[n-1, v] will contain the solution for each v
 def bellman_ford(G, t):
     children = dict()       # keeps track of the first element on the path v -> t
     n = len(G.keys())
@@ -31,7 +33,38 @@ def backtrack(children, s):
         path.append(v)
     return path
 
+# easier version that finds distances to all nodes from a source - same as Dijkstra
+def bellman_ford2(G, s):
+    dist = {}
+    parents = {}
+    for v in G:
+        dist[v] = inf
+        parents[v] = None
+    dist[s] = 0
+    n = len(G.keys())
+    for i in range(n-1):
+        for v in G:
+            if dist[v] == inf:
+                continue
+            for u, wu in G[v]:
+                if dist[u] > dist[v] + wu:
+                    dist[u] = dist[v] + wu
+                    parents[u] = v
+    return dist, parents
+
+def backtrack2(dist, parents, d):
+    if dist[d] == inf:
+        return []   # d is not reachable from s
+    curr = d
+    path = []
+    while curr is not None:
+        path.append(curr)
+        curr = parents[curr]
+    return path[::-1]
+
+
 def main():
+    # refer to ./../Dijkstra/Dijkstra.png for an image of the testing graph
     G = dict()
     G["s"] = [("a", 15), ("b", 6), ("c", 2)]
     G["a"] = []
@@ -39,10 +72,15 @@ def main():
     G["c"] = [("d", 8), ("e", 8)]
     G["d"] = [("a", 3)]
     G["e"] = [("d", 1)]
-    M, children = bellman_ford(G, "d")
-    print(f"M: {M}")
-    print(f"children: {children}")
-    print({v: [M[(i, v)] for i in range(len(G.keys()))] for v in G})
-    path = backtrack(children, "s")
+    # M, children = bellman_ford(G, "d")
+    # print(f"M: {M}")
+    # print(f"children: {children}")
+    # print({v: [M[(i, v)] for i in range(len(G.keys()))] for v in G})
+    # path = backtrack(children, "s")
+    # print(f"path: {path}")
+    dist, parents = bellman_ford2(G, "s")
+    print(f"dist: {dist}")
+    print(f"parents: {parents}")
+    path = backtrack2(dist, parents, "d")
     print(f"path: {path}")
 main()
